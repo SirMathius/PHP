a9b4c5548d432aca9da203ad9062607f3ca7cdf5
SirMathius <mattslightwolf@gmail.com>
2021-01-19 16:04:46 -0300

    practica arreglos (Arrays)

---
+++ Arreglos.php
@@ -0,0 +1,10 @@
+<?php
+//sintaxis normal
+$semama = array('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo');
+//sintaxis nueva (no compatible con archivos php viejos)
+$semana2 = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+
+echo $semama[1];
+
+
+?>
