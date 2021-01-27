bff4d566e9c1d9ebb1adca15d21b4cbc093228a9
SirMathius <mattslightwolf@gmail.com>
2021-01-27 12:24:41 -0300

    Practica Condicional Switch

---
+++ Switch.php
@@ -0,0 +1,20 @@
+<?php
+
+$mes = 'Febrero';
+
+switch($mes) {
+    case 'Diciembre':
+        echo "Feliz Navidad";
+    break;
+
+    case 'Enero':
+        echo "Felix Año Nuevo";
+    break;
+
+    default:
+    echo "En este mes no hay celebraciones";
+    break;
+}
+
+
+?>
