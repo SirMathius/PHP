8df68d24ecafb923e469570f483deaa5454e3354
SirMathius <mattslightwolf@gmail.com>
2021-02-01 15:43:51 -0300

    Practica de parametros en Funciones

---
+++ Funciones/ParametrosEnFunciones.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+function saludo($nombre){
+    echo 'Hola, ' . $nombre;
+    echo '<br />';
+}
+
+saludo('Carlos');
+saludo('Alejandro'); 
+*/
+
+function sumar($numero1, $numero2){
+    $resultado = $numero1 + $numero2;
+    echo $resultado;
+}
+
+sumar(7, 5);
+
+?>
