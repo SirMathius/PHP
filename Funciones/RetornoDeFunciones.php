4d75aae61d74377cf43dfb8d185ea41b8897dd8c
SirMathius <mattslightwolf@gmail.com>
2021-02-04 17:53:29 -0300

    Practica de Return

---
+++ Funciones/RetornoDeFunciones.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function saludo($nombre){
+    return 'Hola, ' . $nombre;
+}
+
+echo saludo('Carlos') . '<br />';
+echo saludo('Alejandro') . '<br />';
+
+/*
+function sumar($numero1, $numero2){
+    $resultado = $numero1 + $numero2;
+    return $resultado;
+}
+
+$resultado = sumar(7, 5);
+*/
+
+?>
