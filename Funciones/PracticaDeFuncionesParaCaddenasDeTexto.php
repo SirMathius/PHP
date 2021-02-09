51aa2b22cb1205285e77580e3efc7e880795ec2c
SirMathius <mattslightwolf@gmail.com>
2021-02-09 15:09:44 -0300

    Practica de Funciones utiles para cadenas de texto

---
+++ Funciones/PracticaDeFuncionesParaCaddenasDeTexto.php
@@ -0,0 +1,14 @@
+<?php
+
+$texto = 'Hola Carlos';
+
+// echo htmlspecialchars($texto);
+// echo trim($texto);
+// echo strlen($texto);
+// echo substr($texto, 0, 4);
+// echo strtouper($texto);
+// echo strolower($texto);
+
+echo strpos($texto, '0');
+
+?>
