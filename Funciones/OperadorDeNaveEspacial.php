ebdfd7f3fbf7ca3db9bada31751d285783615025
SirMathius <mattslightwolf@gmail.com>
2021-02-22 13:23:40 -0300

    Practica de Operador de nave espacial

---
+++ Funciones/OperadorDeNaveEspacial.php
@@ -0,0 +1,12 @@
+<?php
+
+/* <=> (SpaceshipOperator) */
+
+// Regresa 1 si el valor de la izq. es amyor que el de la der.
+// Regresa 0 si ambos valores son iguales
+// Regresa -1 si el valor de la der. es amyor que el de la izq.
+
+// <   =   >
+// 1   0  -1
+
+echo 10 <=> 1;
