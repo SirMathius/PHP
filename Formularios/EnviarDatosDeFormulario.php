b1113f3dad6de0e1662d56ee02605fb723590020
SirMathius <mattslightwolf@gmail.com>
2021-03-03 15:48:00 -0300

    Borrado de archivo innecesario y practica de campos de formulario

--- Formularios/EnviarDatosDeFormulario.php
+++ Formularios/EnviarDatosDeFormulario.php
@@ -6,5 +6,28 @@
 </head>
 <body>
 
+    <form action="recibe.php" methods="">
+        <input type="text" placeholder="Nombre:" name="nombre">
+        <br>
+
+        <label for="hombre">Hombre</label>
+        <input type="radio" name="Sexo" value="hombre" id="hombre">
+        <br>
+
+        <label for="mujer">Mujer</label>
+        <input type="radio" name="Sexo" value="mujer" id="mujer">
+        <br>
+
+        <select name="year" id="year">
+            <option value="2000">2000</option>
+            <option value="2001">2001</option>
+            <option value="2002">2002</option>
+        </select>
+        <br>
+        <label for="terminos">Aceptas los terminos?</label>
+        <input type="checkbox" name="terminos" id="terminos" value="ok">
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
